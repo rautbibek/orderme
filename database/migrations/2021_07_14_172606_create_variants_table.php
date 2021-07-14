ae58c5fb7d9c829922201ab1df0823422e668af1
tnzerox <tnzerox@gmail.com>
2021-07-14 23:25:41 +0545

    product variant table

---
+++ database/migrations/2021_07_14_172606_create_variants_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVariantsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('variants', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('product_id')->unsigned();
+            $table->integer('quantity');
+            $table->integer('onhold');
+            $table->integer('sold');
+            $table->string('code');
+            $table->integer('price');
+            $table->integer('old_price');
+            $table->jsonb('features');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('variants');
+    }
+}
