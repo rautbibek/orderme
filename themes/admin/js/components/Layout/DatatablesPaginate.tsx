7ca9f9aac5eeac6922d04cec1ee3ce6c3d9a4c5d
tnzerox <tnzerox@gmail.com>
2021-08-10 11:24:01 +0545

    product image fix

---
+++ themes/admin/js/components/Layout/DatatablesPaginate.tsx
@@ -0,0 +1,86 @@
+import * as React from 'react';
+import { DataGrid } from '@material-ui/data-grid';
+import Button from '@material-ui/core/Button';
+import {useHistory} from 'react-router-dom'
+import HttpClient from "../../HttpClient";
+import useSWR from 'swr'
+import {makeStyles, IconButton} from "@material-ui/core";
+import {Delete, Edit} from '@material-ui/icons';
+
+
+interface DatatablesProps {
+    url: string
+    columns: any,
+    title: string,
+    extraAction?: any
+    extraRouteButton?: any
+}
+
+const useStyles = makeStyles((theme) => ({
+    buttonWrapper: {
+        display: 'flex',
+        '& IconButton': {
+            marginRight: 10
+        }
+    }
+}));
+
+
+const DataTablesPaginate: React.FC<DatatablesProps> = ({url, columns, title, extraAction, extraRouteButton}) => {
+    const history = useHistory()
+
+    const renderActionsButton = (row) => {
+        const classes = useStyles()
+        const history = useHistory()
+        return (
+            <div className={classes.buttonWrapper}>
+                <IconButton color="primary" onClick={() => history.push(`/${url}/edit/${row.id}`)}>
+                    <Edit />
+                </IconButton>
+                <IconButton color="secondary">
+                    <Delete />
+                </IconButton>
+                {extraAction}
+                {!!extraRouteButton && (
+                    <IconButton color="inherit" onClick={() => history.push(`/${extraRouteButton.url}/${row.id}`)}>
+                        {extraRouteButton.icon}
+                    </IconButton>
+                )}
+            </div>
+        )
+    }
+
+    const columnsArray = [
+        ...columns,
+        {
+            field: '',
+            headerName: 'Action',
+            flex: 0.3,
+            renderCell: renderActionsButton,
+            disableClickEventBubbling: true,
+            sortable: false,
+        }
+    ]
+
+    const fetchData = async () => {
+       return await HttpClient.get(url)
+    }
+
+    const { data: data, error } = useSWR(`${url}`, fetchData, {revalidateOnFocus: false, revalidateOnReconnect: false} )
+
+    if (error) return <div>failed to load</div>
+    if (!data) return <div>loading...</div>
+
+    return (
+        <div style={{ width: '100%' }}>
+            <Button variant="contained" onClick={() => history.push(`/${title}/new`)} color="primary" style={{marginBottom: 10}}>
+                New
+            </Button>
+            <div>
+                <DataGrid rows={data.data} columns={columnsArray} autoHeight hideFooterPagination hideFooter disableSelectionOnClick />
+            </div>
+        </div>
+    );
+}
+
+export default DataTablesPaginate
--- themes/admin/js/components/pages/product/Product.tsx
+++ themes/admin/js/components/pages/product/Product.tsx
@@ -3,7 +3,19 @@ import Datatables from '../../Layout/Datatables';
 import { listProduct } from '../../../createUrls';
 
 const columns = [
+
     { field: 'title', headerName: 'Title', flex: 1 },
+    {
+        field: 'image',
+        headerName: 'Image',
+        renderCell: (params) => (
+            <div style={{display: "flex", justifyContent: "center", width: '100%'}}>
+                <img src={params.value[0]} alt="" style={{height: 50}}/>
+            </div>
+        ),
+        width: 150
+
+    },
 ];
 
 const Product = () => {
