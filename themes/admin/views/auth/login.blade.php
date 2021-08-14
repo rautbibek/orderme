1a949dccc7b4d87249f3b4d261f80fccdaa63ac1
tnzerox <tnzerox@gmail.com>
2021-08-14 13:45:15 +0545

    aiska bg vayera design yesto vo

--- themes/admin/views/auth/login.blade.php
+++ themes/admin/views/auth/login.blade.php
@@ -1,71 +1,74 @@
 <x-app-layout>
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">{{ __('Login') }}</div>
+<div class="wrapper">
+<div class="login-container">
+  
+<div class="login-card">
+<div class="image-container" style="margin:auto">
+    <img class="img" src="https://firebasestorage.googleapis.com/v0/b/tradekunj.appspot.com/o/58d13329-6f7b-420a-ad0f-2937c6a93a2c-225835407_110499844660397_3299088769491264290_n.png?alt=media&token=991baa7a-2041-483e-838d-e476b344fb14" alt="">
+</div>
+<div class="header-title">{{ __('Login') }}</div>
 
-                    <div class="card-body">
-                        <form method="POST" action="{{ route('admin.login') }}">
-                            @csrf
+<div class="card-body">
+    <form method="POST" action="{{ route('admin.login') }}">
+        @csrf
 
-                            <div class="form-group row">
-                                <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+        <div class="group">
+            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
-                                <div class="col-md-6">
-                                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+            <div class="col-md-6">
+                <input id="email" type="email" class="custom-textbox @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
-                                    @error('email')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror
-                                </div>
-                            </div>
+                @error('email')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
+            </div>
+        </div>
 
-                            <div class="form-group row">
-                                <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+        <div class="group">
+            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
-                                <div class="col-md-6">
-                                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+            <div class="col-md-6">
+                <input id="password" type="password" class="custom-textbox @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
 
-                                    @error('password')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror
-                                </div>
-                            </div>
+                @error('password')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
+            </div>
+        </div>
 
-                            <div class="form-group row">
-                                <div class="col-md-6 offset-md-4">
-                                    <div class="form-check">
-                                        <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+        <div class="group">
+            <div class="group">
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
-                                        <label class="form-check-label" for="remember">
-                                            {{ __('Remember Me') }}
-                                        </label>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="mb-0 form-group row">
-                                <div class="col-md-8 offset-md-4">
-                                    <button type="submit" class="btn btn-primary">
-                                        {{ __('Login') }}
-                                    </button>
-
-                                    @if (Route::has('password.request'))
-                                        <a class="btn btn-link" href="{{ route('password.request') }}">
-                                            {{ __('Forgot Your Password?') }}
-                                        </a>
-                                    @endif
-                                </div>
-                            </div>
-                        </form>
-                    </div>
+                    <label class="form-check-label" for="remember">
+                        {{ __('Remember Me') }}
+                    </label>
                 </div>
             </div>
         </div>
-    </div>
+
+        <div class="mb-0 form-group row" style="text-align:center">
+            <div class="col-md-8 offset-md-4">
+                <button type="submit" class="login-button">
+                    {{ __('Login') }}
+                </button>
+
+                <!-- @if (Route::has('password.request'))
+                    <a class="btn btn-link" style="padding:10px"  href="{{ route('password.request') }}">
+                        {{ __('Forgot Your Password?') }}
+                    </a>
+                @endif -->
+            </div>
+        </div>
+    </form>
+</div>
+</div>                  
+</div>
+</div>
+
 </x-app-layout>
--- themes/admin/views/layouts/app.blade.php
+++ themes/admin/views/layouts/app.blade.php
@@ -3,11 +3,93 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500&display=swap" rel="stylesheet">
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{'Admin'}}</title>
+    <style>
+        body{
+            margin:0px;
+            padding:0px;
+            font-family:Roboto,sans-serif;
+        }
+        .image-container{
+            height:50px;
+            width:150px;
+            border-radius:50%;
+            background:#cdd1d6;
+            position:relative;
+            
+            
+        }
+        .img{
+            position:absolute;
+            top:-120px;
+            height:150px;
+            width:150px;
+        }
+        .wrapper{
+            display:flex;
+            justify-content:center;
+            align-items:center;
+            background:#eaeff5;
+            height:100vh;
+        }
+        .login-container {
+            display: flex;
+            justify-content: center;
+            padding:20px;
+            
+            border-radius: 10px;
+            box-shadow: 32px 81px 357px 200px rgba(33,33,33,0.19);
+            -webkit-box-shadow: 32px 81px 357px 200px rgba(33,33,33,0.19);
+            -moz-box-shadow: 32px 81px 357px 200px rgba(33,33,33,0.19);
+            
+        }
+        .login-card {
+            
+            
+        }
+        .custom-textbox{
+            height:32px;
+            width:350px;
+            padding:5px;
+            border-radius:2px;
+            border:1px solid black;
+            margin:10px 0px 10px 0px;
+
+            
+        }
+        .custom-textbox:focus{
+            border:1px solid black;
+        }
+        .custom-textbox:focus {
+            border:none;
+        }
+        
+       .login-button{
+           height:40px;
+           width:100%;
+           margin-top:20px;
+           border-radius:10px;
+           border:none;
+           background-color:green;
+           color:white;
+
+       }
+       .header-title{
+           font-size:18px;
+           font-weight:500;
+           text-align:center;
+           margin-top:20px;
+       }
+       .group{
+           width:100%;
+       }
+    </style>
 
     <!-- Scripts -->
     <script src="{{ asset('themes/admin/js/app.js') }}" defer></script>
