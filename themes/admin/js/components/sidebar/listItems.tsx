8133269b229e22b57db3b21950edeb0a05a3e784
tnzerox <tnzerox@gmail.com>
2021-08-14 12:01:59 +0545

    sidebar css fix

--- themes/admin/js/components/sidebar/listItems.tsx
+++ themes/admin/js/components/sidebar/listItems.tsx
@@ -10,6 +10,7 @@ import BarChartIcon from '@material-ui/icons/BarChart';
 import LayersIcon from '@material-ui/icons/Layers';
 import AssignmentIcon from '@material-ui/icons/Assignment';
 import {Link} from 'react-router-dom';
+import './listItem.css'
 
 export const mainListItems = (
   <div>
