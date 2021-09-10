5a869dac475317310d5b0ee49bbebb53ab3b323a
tnzerox <tnzerox@gmail.com>
2021-09-10 16:27:41 +0545

    fix

--- themes/admin/views/layouts/app.blade.php
+++ themes/admin/views/layouts/app.blade.php
@@ -13,6 +13,87 @@
 
     <!-- Scripts -->
     <script src="{{ asset('themes/admin/js/app.js') }}" defer></script>
+    <style>
+        body{
+            margin:0px;
+            padding:0px;
+            font-family:Roboto,sans-serif;
+        }
+        .image-container{
+            height:50px;
+            width:150px;
+            border-radius:50%;
+            background:#cdd1d6;
+            position:relative;
+
+
+        }
+        .img{
+            position:absolute;
+            top:-120px;
+            height:150px;
+            width:150px;
+        }
+        .wrapper{
+            display:flex;
+            justify-content:center;
+            align-items:center;
+            background:#eaeff5;
+            height:100vh;
+        }
+        .login-container {
+            display: flex;
+            justify-content: center;
+            padding:20px;
+
+            border-radius: 10px;
+            box-shadow: 32px 81px 357px 200px rgba(33,33,33,0.19);
+            -webkit-box-shadow: 32px 81px 357px 200px rgba(33,33,33,0.19);
+            -moz-box-shadow: 32px 81px 357px 200px rgba(33,33,33,0.19);
+
+        }
+        .login-card {
+
+
+        }
+        .custom-textbox{
+            height:32px;
+            width:350px;
+            padding:5px;
+            border-radius:2px;
+            border:1px solid black;
+            margin:10px 0px 10px 0px;
+
+
+        }
+        .custom-textbox:focus{
+            border:1px solid black;
+        }
+        .custom-textbox:focus {
+            border:none;
+        }
+
+        .login-button{
+            height:40px;
+            width:100%;
+            margin-top:20px;
+            border-radius:10px;
+            border:none;
+            background-color:green;
+            color:white;
+
+        }
+        .header-title{
+            font-size:18px;
+            font-weight:500;
+            text-align:center;
+            margin-top:20px;
+        }
+        .group{
+            width:100%;
+        }
+    </style>
+
 
 </head>
 <body>
