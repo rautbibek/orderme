b3084d7b25ea38318d20733b670f43a0c000ac10
tnzerox <tnzerox@gmail.com>
2021-09-28 16:45:51 +0545

    fix

--- themes/frontend/views/themes/molla/template/collection.blade.php
+++ themes/frontend/views/themes/molla/template/collection.blade.php
@@ -19,26 +19,26 @@
         <div class="container">
             <div class="row">
                 <div class="col-lg-9">
-                    <div class="toolbox">
-                        <div class="toolbox-left">
-                            <div class="toolbox-info">
-                                Showing <span>9 of 56</span> Products
-                            </div><!-- End .toolbox-info -->
-                        </div><!-- End .toolbox-left -->
+{{--                    <div class="toolbox">--}}
+{{--                        <div class="toolbox-left">--}}
+{{--                            <div class="toolbox-info">--}}
+{{--                                Showing <span>9 of 56</span> Products--}}
+{{--                            </div><!-- End .toolbox-info -->--}}
+{{--                        </div><!-- End .toolbox-left -->--}}
 
-                        <div class="toolbox-right">
-                            <div class="toolbox-sort">
-                                <label for="sortby">Sort by:</label>
-                                <div class="select-custom">
-                                    <select name="sortby" id="sortby" class="form-control">
-                                        <option value="popularity" selected="selected">Most Popular</option>
-                                        <option value="rating">Most Rated</option>
-                                        <option value="date">Date</option>
-                                    </select>
-                                </div>
-                            </div><!-- End .toolbox-sort -->
-                        </div><!-- End .toolbox-right -->
-                    </div><!-- End .toolbox -->
+{{--                        <div class="toolbox-right">--}}
+{{--                            <div class="toolbox-sort">--}}
+{{--                                <label for="sortby">Sort by:</label>--}}
+{{--                                <div class="select-custom">--}}
+{{--                                    <select name="sortby" id="sortby" class="form-control">--}}
+{{--                                        <option value="popularity" selected="selected">Most Popular</option>--}}
+{{--                                        <option value="rating">Most Rated</option>--}}
+{{--                                        <option value="date">Date</option>--}}
+{{--                                    </select>--}}
+{{--                                </div>--}}
+{{--                            </div><!-- End .toolbox-sort -->--}}
+{{--                        </div><!-- End .toolbox-right -->--}}
+{{--                    </div><!-- End .toolbox -->--}}
 
                     <div class="products mb-3">
                         <div class="row justify-content-center">
