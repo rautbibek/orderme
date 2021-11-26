df50c2e17289d8d5f2e7104d478f7e023850ff87
Tnz <tnzerox@gmail.com>
2021-11-26 11:38:01 +0545

    fix

--- themes/admin/js/HttpClient.ts
+++ themes/admin/js/HttpClient.ts
@@ -1,7 +1,7 @@
 import Axios from "axios";
 
 const HttpClient = Axios.create({
-    baseURL: process.env.MIX_APP_URL === 'prod' ? 'https://tradekunj.com/admin/' : `http://orderme.test/admin/`
+    baseURL: process.env.MIX_APP_URL === 'prod' ? 'https://tradekunj.com/admin/' : `/admin/`
 })
 
 HttpClient.interceptors.response.use(
