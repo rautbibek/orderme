874ecb9b3c7ac9892b521ceec1269ffb9e7f540f
jhapali9 <kaflevola@gmail.com>
2021-12-13 11:13:00 +0545

    service detail design changes

--- themes/frontend/views/themes/molla/template/expert.blade.php
+++ themes/frontend/views/themes/molla/template/expert.blade.php
@@ -10,18 +10,21 @@
             <div class="row">
                 <div class="col-sm-5 col-xl-6">
                     <figure class="store-media mb-2 mb-lg-0">
-                        <img src="{{$expert->image}}" alt="image">
+                        <img src="{{$expert->image}}" alt="image" style="max-height: 35rem;min-height:35rem">
                     </figure><!-- End .store-media -->
                 </div><!-- End .col-xl-6 -->
                 <div class="col-sm-7 col-xl-6">
-                    <div class="store-content">
+                    <div class="store-content px-4 py-4" style="height:35rem;overflow:hidden">
                         <h3 class="store-title">About {{$expert->name}}</h3><!-- End .store-title -->
                         <p>{!! $expert->description !!}</p>
                     </div><!-- End .store-content -->
+                    <a href="javascript:void(0)" class="px-4 py-4" id="read-more">Read More <i class="icon-long-arrow-right"></i></a>
                 </div><!-- End .col-xl-6 -->
             </div>
         </div>
 
+        <hr>
+        
         <div class="page-content pb-0 mt-2">
             <div class="container">
                 <div class="row">
@@ -33,6 +36,10 @@
                                     <h3>Personal Info</h3>
 
                                     <ul class="contact-list">
+                                        <li>
+                                            <i class="icon-user"></i>
+                                            {{$expert->name}}
+                                        </li>
                                         <li>
                                             <i class="icon-map-marker"></i>
                                             {{$expert->address}}, {{$expert->province}}, {{$expert->city}}
@@ -56,7 +63,7 @@
                                     <ul class="contact-list">
                                         <li>
                                             <i class="icon-cog"></i>
-                                            <span class="text-dark">{{$expert->service->title}}</span> <br>Electrician
+                                            <span class="text-dark">{{$expert->service->title}}</span> <br>
                                         </li>
                                         <li>
                                             <i class="icon-calendar"></i>
@@ -65,12 +72,48 @@
                                     </ul>
                                 </div>
                             </div>
+
+                            <div class="col-sm-4">
+                            <div class="contact-info">
+                                <h3>Experiences</h3>
+
+                                <ul class="contact-list">
+                                    <li>
+                                        <i class="icon-clock-o"></i>
+                                        <span class="text-dark">Service Period</span> <br>2 years+
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+
                         </div>
                     </div>
                 </div>
             </div>
         </div>
 
+        <hr>
+
     </div>
 
+@endsection
+
+@section('js')
+    <script>
+        
+        $('#read-more').click(function(e) {
+            e.stopPropagation();
+            $('.store-content').css({
+                'height': 'auto'
+            })
+        });
+
+        $(document).click(function() {
+            $('.store-content').css({
+                'height': '35rem'
+            })
+        })
+
+
+    </script>
 @endsection
