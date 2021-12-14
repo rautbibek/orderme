4317d4151dae5bcdff40b0f805916a6961982eb5
tnzerox <tnzerox@gmail.com>
2021-12-15 02:16:35 +0545

    fix

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -26,7 +26,7 @@ class RedirectIfAuthenticated
                 if($guard === 'admin'){
                     return redirect()->route('admin.dashboard');
                 }
-                return redirect(RouteServiceProvider::HOME);
+                return redirect()->route('user.dashboard');
             }
         }
 
